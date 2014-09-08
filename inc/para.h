d8e30d86ebe676d168d8a6b5545ec83b646688e3
Tian-Min Yan <timy.tech@gmail.com>
2014-09-08 09:10:28 +0200

    indentation

--- inc/para.h
+++ inc/para.h
@@ -15,199 +15,199 @@ namespace std { template<typename T> class complex; }
 
 struct para_bath
 {
-  double g;
-  double w_cut;
-  double T;
+    double g;
+    double w_cut;
+    double T;
 
-  double g12;
-  double g21;
-  double G11;
-  double G22;
-  double G12;
-  double G10;
-  double G20;
-  double G31;
-  double G32;
-  double G30;
+    double g12;
+    double g21;
+    double G11;
+    double G22;
+    double G12;
+    double G10;
+    double G20;
+    double G31;
+    double G32;
+    double G30;
 };
 
 struct para_repr
 {
-  double theta;
+    double theta;
 };
 
 struct para_coord
 {
-  double phi;
-  double theta;
-  double psi;
-  // n_dim * n_dim
-  double **mxRotF;
-  double **mxRotB;
+    double phi;
+    double theta;
+    double psi;
+    // n_dim * n_dim
+    double **mxRotF;
+    double **mxRotB;
 };
 
 struct para_ef
 {   // units in a.u.
 
     // set by user
-  double W;
-  double E0;
-  double FWHM;
+    double W;
+    double E0;
+    double FWHM;
 
-  // Euler angles for k, and relative angle for E
-  double *kdir;
-  double Edir;
+    // Euler angles for k, and relative angle for E
+    double *kdir;
+    double Edir;
 
-  double tc;
-  // filled automatically
-  double ph;
-  double k0;
-  double *kuvL;
-  double *EuvL;
-  double *kuvM;
-  double *EuvM;
+    double tc;
+    // filled automatically
+    double ph;
+    double k0;
+    double *kuvL;
+    double *EuvL;
+    double *kuvM;
+    double *EuvM;
 };
 
 struct para_esmb
 {
-  long n_esmb;
-  void* rng; // the damn gsl_rng typedef
-  int with_old;
+    long n_esmb;
+    void* rng; // the damn gsl_rng typedef
+    int with_old;
 };
 
 struct para_mvar
 {
-  long ny;
-  double y0;
-  double y1;
-  double dy;
-  int dimGrid;
+    long ny;
+    double y0;
+    double y1;
+    double dy;
+    int dimGrid;
 };
 
 struct para_mpic
 {
-  long size;
-  long rank; // rank of the current unit
-  long njob; // number of job for the current unit
-  long idx0; // first index for the current unit
+    long size;
+    long rank; // rank of the current unit
+    long njob; // number of job for the current unit
+    long idx0; // first index for the current unit
 };
 
 typedef struct _IO_FILE FILE;
 
 #include <map>
 struct para_file {
-  enum file_type {
-    DM,
-    EF,
-    TIME,
-    INFO,
-    GRID_1D,
-    GRID_2D,
-    PPAR_1D,
-    PTOT_1D,
-    SPEC_1D,
-    PPAR_2D,
-    PTOT_2D,
-    SPEC_2D,
+    enum file_type {
+        DM,
+        EF,
+        TIME,
+        INFO,
+        GRID_1D,
+        GRID_2D,
+        PPAR_1D,
+        PTOT_1D,
+        SPEC_1D,
+        PPAR_2D,
+        PTOT_2D,
+        SPEC_2D,
 
-    CONFIG,
-    ORIENT,
-    KUVL,
-    KUVM,
-    KL,
-    KM,
-    EUVL,
-    EUVM,
-    RL,
-    RM,
+        CONFIG,
+        ORIENT,
+        KUVL,
+        KUVM,
+        KL,
+        KM,
+        EUVL,
+        EUVM,
+        RL,
+        RM,
 
-    LOG
-  };
+        LOG
+    };
 
-  struct one_node {
-    int n_file;
-    char* name;
-    FILE* fptr;
-  };
+    struct one_node {
+        int n_file;
+        char* name;
+        FILE* fptr;
+    };
 
-  struct mul_node {
-    int n_file;
-    char** name;
-    FILE** fptr;
-  };
+    struct mul_node {
+        int n_file;
+        char** name;
+        FILE** fptr;
+    };
 
-  std::map<file_type, one_node*> one;
-  std::map<file_type, mul_node*> mul;
+    std::map<file_type, one_node*> one;
+    std::map<file_type, mul_node*> mul;
 };
 
 struct para_seidner
 {
-  int n_phase;
+    int n_phase;
 
-  // n_phase * n_dim
-  double **rL;
-  double **rM;
+    // n_phase * n_dim
+    double **rL;
+    double **rM;
 
-  // n_phase * n_pulse
-  int **index;
-  double **phiL;
-  double **phiM;
+    // n_phase * n_pulse
+    int **index;
+    double **phiL;
+    double **phiM;
 
-  // n_phase * n_phase
-  std::complex<double> **pnl_expr;
+    // n_phase * n_phase
+    std::complex<double> **pnl_expr;
 
-  void* rng; // the damn gsl_rng typedef
+    void* rng; // the damn gsl_rng typedef
 };
 
 struct para_help
 {
-  double w_max;
+    double w_max;
 };
 
 struct parameters
 {
-  int n_dim;
-  int n_lvl;
-  int n_dpl;
-  int n_eom;
+    int n_dim;
+    int n_lvl;
+    int n_dpl;
+    int n_eom;
 
-  int n_pulse;
+    int n_pulse;
 
-  double *energy;      // energy
-  double **dipole;     // transition dipole
-  double *pos;         // position in lab frame
+    double *energy;      // energy
+    double **dipole;     // transition dipole
+    double *pos;         // position in lab frame
 
-  double *time;
+    double *time;
 
-  // parameters
-  // double k0;
-  // double Om;
-  double tau;
-  double T;
-  double t;
-  double t0;
-  double t1;
-  double dt;
-  long nt;
-  long it0;
-  double d_t;
-  long n_t;
+    // parameters
+    // double k0;
+    // double Om;
+    double tau;
+    double T;
+    double t;
+    double t0;
+    double t1;
+    double dt;
+    long nt;
+    long it0;
+    double d_t;
+    long n_t;
 
-  // sub-systems
-  struct para_repr *repr;
-  struct para_bath *bath;
-  struct para_coord *coord;
-  struct para_ef **ef;
-  struct para_esmb *esmb;
-  struct para_mvar *mvar;
-  struct para_mpic *mpic;
-  struct para_file *file;
-  struct para_ppar *ppar;
-  struct para_seidner *seid;
-  struct para_help *help;
+    // sub-systems
+    struct para_repr *repr;
+    struct para_bath *bath;
+    struct para_coord *coord;
+    struct para_ef **ef;
+    struct para_esmb *esmb;
+    struct para_mvar *mvar;
+    struct para_mpic *mpic;
+    struct para_file *file;
+    struct para_ppar *ppar;
+    struct para_seidner *seid;
+    struct para_help *help;
 
-  // function pointer to EOM
-  int (*f_eom)( double, const double*, double*, void* );
+    // function pointer to EOM
+    int (*f_eom)( double, const double*, double*, void* );
 };
 
 void para_ini( struct parameters *ps, const char* file_name,
