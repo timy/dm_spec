ce4216462ad27991e6412298dd3886be9830d5fb
Tianmin Yan <tianmin@pinot.dynmol.chim.ulg.ac.be>
2014-09-15 16:32:55 +0200

    convention ...

--- ana/pullerits/find_max.py
+++ ana/pullerits/find_max.py
@@ -5,38 +5,38 @@ fileDir.close()
 order = ['1', '3', '5'];
 
 idxStart = [0, 6, 44]
-n_order, n_coo, nt = 2, 3, 960
-n_dir = [len(idxDir[order[i]]) for i in range(n_order)]
+nOrder, nCoo, nt = 2, 3, 960
+nDir = [len(idxDir[order[i]]) for i in range(nOrder)]
 
 import numpy as np
 # calculate the abs value of emitting electric field (including all directions)
-def calcAmplitude(i_order, i_dir):
+def calcAmplitude(iOrder, iDir):
     s = np.zeros(nt)
-    for i_coo in range(n_coo):
-        data = np.loadtxt( "res_%d/ppar_2d_dpl0_coo%d_%d_0.dat" % (i_order, i_coo, i_dir))
+    for iCoo in range(nCoo):
+        data = np.loadtxt( "res_%d/ppar_2d_dpl0_coo%d_%d_0.dat" % (iOrder, iCoo, iDir))
         for i in range(2):
             s += data[:,i]**2
     return np.sqrt(s)
 # this function is not used, the index of the direction l= [l1, l2, l3]
 def indexOfDir( idxDir, l ):
-    for i_order in range(n_order):
-         for index, item in enumerate(idxDir[order[i_order]]):
+    for iOrder in range(nOrder):
+         for index, item in enumerate(idxDir[order[iOrder]]):
             if item == l:
-                return i_order, index
+                return iOrder, index
 # calculate the largest value of the ith-order / jth-direction
-maxAmp = np.array( [np.zeros(n_dir[i]) for i in range(n_order)] )
-for i_order in range(n_order):
-    for i_dir in range(n_dir[i_order]):
-        maxAmp[i_order][i_dir] = np.amax( calcAmplitude(i_order, i_dir) )
-        print( "%d_%d: %le" % (i_order, i_dir, maxAmp[i_order][i_dir]) )
+maxAmp = np.array( [np.zeros(nDir[i]) for i in range(nOrder)] )
+for iOrder in range(nOrder):
+    for iDir in range(nDir[iOrder]):
+        maxAmp[iOrder][iDir] = np.amax( calcAmplitude(iOrder, iDir) )
+        print( "%d_%d: %le" % (iOrder, iDir, maxAmp[iOrder][iDir]) )
 # plot
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 fig, ax = plt.subplots(2)
-for i_order in range(n_order):
-    ax[i_order].plot(maxAmp[i_order], marker='.')
-    ax[i_order].grid(True)
-    ax[i_order].xaxis.set_major_locator(MaxNLocator(n_dir[i_order]-1))
-    ax[i_order].set_xticks(range(n_dir[i_order]))
-    ax[i_order].set_xticklabels(idxDir[order[i_order]], rotation='90')
+for iOrder in range(nOrder):
+    ax[iOrder].plot(maxAmp[iOrder], marker='.')
+    ax[iOrder].grid(True)
+    ax[iOrder].xaxis.set_major_locator(MaxNLocator(nDir[iOrder]-1))
+    ax[iOrder].set_xticks(range(nDir[iOrder]))
+    ax[iOrder].set_xticklabels(idxDir[order[iOrder]], rotation='90')
 plt.show()
--- ana/seidner/find_max.py
+++ ana/seidner/find_max.py
@@ -1,35 +1,35 @@
 import json
 fileDir = open('../../cfg/index.json')
-idxDir = json.load(fileDir)['order']['1']
+idxDir = json.load(fileDir)['order']
 fileDir.close()
+order = ['1', '3', '5']
 
-# idxStart = 0, 6, 44 for 1st, 3rd and 5th order respectively
-idxStart = 0
-n_coo, nt = 3, 960
-nDir = len(idxDir)
+iOrder = 1;
+idxStart = [0, 6, 44]
+nCoo, nt = 3, 960
+nDir = len(idxDir[order[iOrder]])
 
 import numpy as np
-
 # calculate the abs value of emitting electric field (including all directions)
 def calcAmplitude(iDir):
     s = np.zeros(nt)
-    for i_coo in range(n_coo):
-        data = np.loadtxt( "res/ppar_2d_dpl0_coo%d_%d_0.dat" % (i_coo, iDir))
+    for iCoo in range(nCoo):
+        data = np.loadtxt( "res/ppar_2d_dpl0_coo%d_%d_0.dat" % (iCoo, iDir))
         for i in range(2):
             s += data[:,i]**2
     return np.sqrt(s)
 # this function is not used, the index of the direction l= [l1, l2, l3]
 def indexOfDir( idxDir, l ):
-    # for i_order in range(n_order):
+    # for iOrder in range(nOrder):
     for index, item in enumerate(idxDir):
         if item == l:
             return index
 # calculate the largest value of the ith-order / jth-direction
-# maxAmp = np.array( [np.zeros(nDir[i]) for i in range(n_order)] )
+# maxAmp = np.array( [np.zeros(nDir[i]) for i in range(nOrder)] )
 maxAmp = np.zeros(nDir)
-# for i_order in range(n_order):
+# for iOrder in range(nOrder):
 for iDir in range(nDir):
-    maxAmp[iDir] = np.amax( calcAmplitude(idxStart+iDir) )
+    maxAmp[iDir] = np.amax( calcAmplitude(idxStart[iOrder]+iDir) )
     print( "%d: %le" % (iDir, maxAmp[iDir]) )
 # plot
 import matplotlib.pyplot as plt
@@ -37,10 +37,10 @@ from matplotlib.ticker import MaxNLocator
 #fig, ax = plt.subplots(2)
 fig = plt.figure()
 ax = fig.add_subplot( 1, 1, 1 )
-#for i_order in range(n_order):
+#for iOrder in range(nOrder):
 ax.plot(maxAmp, marker='.')
 ax.grid(True)
 ax.xaxis.set_major_locator(MaxNLocator(nbins=nDir-1, integer=True))
 ax.set_xticks(range(nDir))
-ax.set_xticklabels(idxDir, rotation='90')
+ax.set_xticklabels(idxDir[order[iOrder]], rotation='90')
 plt.show()
